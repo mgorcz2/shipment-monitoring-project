8315e25feba8dfceae81c8015d0d85f7d433204f
mgorczynsky2 <mgorczynsky2@gmail.com>
2024-11-18 12:29:58 +0100

    ignore .idea folder for github

--- shipment-api/shipment_monitoring/core/domain/shipment.py
+++ shipment-api/shipment_monitoring/core/domain/shipment.py
@@ -4,13 +4,13 @@ from typing import Optional
 from pydantic import BaseModel, ConfigDict
 
 
-class ShipmentIn(BaseModel):
+class ShipmentIn(BaseModel): #ShipmentIn zawiera tylko te dane, które sa wymagane do stworzenia
     origin: str
     destination: str
     weight: float
     status: str
 
-class Shipment(ShipmentIn):
+class Shipment(ShipmentIn):     #pelen obiekt
     id: int
 
     model_config = ConfigDict(from_attributes=True, extra='ignore')
