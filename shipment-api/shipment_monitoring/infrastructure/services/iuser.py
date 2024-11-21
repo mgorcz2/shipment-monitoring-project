1d6a1d58dfc0c2b295417500a92c217b2b1c30e4
mgorczynsky2 <mgorczynsky2@gmail.com>
2024-11-21 22:42:46 +0100

    added login verification
    login with password hashing
    next step jwt token

--- shipment-api/shipment_monitoring/infrastructure/services/iuser.py
+++ shipment-api/shipment_monitoring/infrastructure/services/iuser.py
@@ -4,9 +4,13 @@ from shipment_monitoring.core.domain.user import User,UserIn
 
 class IUserService(ABC):
     @abstractmethod
-    async def add_user(self,User: UserIn) -> User | None:
+    async def register_user(self,User: UserIn) -> User | None:
         pass
 
     @abstractmethod
     async def get_user_by_id(self, user_id) -> User | None:
+        pass
+
+    @abstractmethod
+    async def login_user(self, login, password) -> User | None:
         pass
