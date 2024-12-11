818e7c9a7a1cabf61abc3151caccf0d8cc207bb6
Marcin G <mgorczynsky2@gmail.com>
2024-12-11 17:36:21 +0100

    Add doc for some endpoints

--- shipment-api/shipment_monitoring/api/routers/user.py
+++ shipment-api/shipment_monitoring/api/routers/user.py
@@ -19,29 +19,42 @@ async def register_user(
         new_user: UserIn,
         service: IUserService = Depends(Provide[Container.user_service]),
 ) -> dict:
+    """An endpoint for registering new user.
+
+    Args:
+        new_user (UserIn): The user input data.
+        service (IUserService): The injected user service.
+
+    Raises:
+        HTTPException: _description_
+
+    Returns:
+        dict: The user DTO details.
+    """
     try:
         user = await service.register_user(new_user)
         return user.model_dump()
     except ValueError as error:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(error))
-    
-@router.get("/get/{username}/", response_model=User, status_code=200)
-@inject
-async def get_user_by_username(
-    username: str,
-    service: IUserService = Depends(Provide[Container.user_service]),
-) -> dict | None:
-    if user := await service.get_user_by_username(username):
-        return user
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
-        
-    
+     
 @router.post("/token", response_model=TokenDTO)
 @inject
 async def login_for_access_token(
     form_data: OAuth2PasswordRequestForm = Depends(),
     service: IUserService = Depends(Provide[Container.user_service])
 ):
+    """An endpoint for authenticating users(creating token)
+
+    Args:
+        form_data (OAuth2PasswordRequestForm, optional): The user input data from request form.
+        service (IUserService): The injected user service.
+
+    Raises:
+        HTTPException: _description_
+
+    Returns:
+        _type_: _description_
+    """
     try:
         token = await service.login_for_access_token(form_data.username, form_data.password)
         return token
