2ff6da41b49772b05a0fa607497184ae1d7bda16
mgorczynsky2 <mgorczynsky2@gmail.com>
2024-12-12 14:02:21 +0100

    documentation

--- shipment-api/shipment_monitoring/infrastructure/services/iuser.py
+++ shipment-api/shipment_monitoring/infrastructure/services/iuser.py
@@ -1,21 +1,54 @@
+"""Module containing user service abstractions."""
+
 from abc import ABC, abstractmethod
 from shipment_monitoring.core.domain.user import User,UserIn
 from shipment_monitoring.infrastructure.dto.tokenDTO import TokenDTO
 from uuid import UUID
 class IUserService(ABC):
+    """An abstract class representing the protocol of user service."""
+    
     @abstractmethod
     async def register_user(self,User: UserIn) -> User | None:
-        pass
+        """The abstract method for registering a new user in repository.
+
+        Args:
+            user (UserIn): The user input data to register.
+
+        Returns:
+            User | None: The registered user object if successful, None otherwise.
+        """
 
     @abstractmethod
     async def get_user_by_id(self, user_id:UUID) -> User | None:
-        pass
+        """The abstract getting user by provided id from repository.
+
+        Args:
+            user_id (UUID): UUID of the user.
+
+        Returns:
+            User | None: The user object if exists.
+        """
     
     @abstractmethod
     async def get_user_by_username(self,username) -> User | None:
-        pass
+        """The abstract getting user by provided username from repository.
+
+        Args:
+            username (str): The username of the user.
+
+        Returns:
+            User | None: The user object if exists.
+        """
     
     @abstractmethod
-    async def login_for_access_token(self, login, password) -> TokenDTO | None:
-        pass
+    async def login_for_access_token(self, username: str, password: str) -> TokenDTO | None:
+        """The abstract method for user authentication to get an access token.
+
+        Args:
+            username (str): The login identifier for the user
+            password (str): The user's password for authentication.
+
+        Returns:
+            TokenDTO | None: A token DTO if login is successful, None otherwise.
+        """
     
--- shipment-api/shipment_monitoring/infrastructure/services/user.py
+++ shipment-api/shipment_monitoring/infrastructure/services/user.py
@@ -1,3 +1,6 @@
+"""Module containing user service implementation."""
+
+
 from shipment_monitoring.core.domain.user import User, UserIn
 from shipment_monitoring.infrastructure.dto.userDTO import UserDTO
 from shipment_monitoring.infrastructure.services.iuser import IUserService
@@ -11,12 +14,24 @@ from shipment_monitoring.core.security import password_hashing
 from uuid import UUID
 
 class UserService(IUserService):
+    """A class representing implementation of user-related services."""
     _repository: IUserRepository
 
     def __init__(self, repository: IUserRepository):
         self._repository = repository
 
     async def register_user(self, user: UserIn) -> UserDTO | None:
+        """The method for registering a new user in repository.
+
+        Args:
+            user (UserIn): The user input data.
+
+        Returns:
+            UserDTO | None: The user DTO details if exists.
+        
+        Raises:
+            ValueError: If the username is already registered.
+        """
         existing_user = await self._repository.get_user_by_username(user.username)
         if existing_user:
             raise ValueError("Username already registered")
@@ -24,12 +39,41 @@ class UserService(IUserService):
         return await self._repository.register_user(user)
 
     async def get_user_by_id(self, user_id:UUID) -> UserDTO | None:
+        """The method getting user by provided id from repository.
+
+        Args:
+            user_id (UUID): UUID of the user.
+
+        Returns:
+            User | None: The user DTO details if exists.
+        """
         return await self._repository.get_user_by_id(user_id)
     
     async def get_user_by_username(self,username) -> User | None:
+        """The method getting user by provided username from repository.
+
+        Args:
+            username (str): The username of the user.
+
+        Returns:
+            User | None: The user object if exists.
+        """
         return await self._repository.get_user_by_username(username)
 
-    async def login_for_access_token(self, username, password) -> TokenDTO | None:
+    async def login_for_access_token(self, username: str, password: str) -> TokenDTO | None:
+        """The method for user authentication to get an access token.
+
+        Args:
+            username (str): The login identifier for the user
+            password (str): The user's password for authentication.
+
+        Returns:
+            TokenDTO | None: A token DTO if login is successful, None otherwise.
+            
+        Raises:
+            ValueError: If the users data is invalid.
+        """
+
         user = await self._repository.get_user_by_username(username=username)
         if not user or not password_hashing.verify_password(password, user.password):
             raise ValueError("Incorrect username or password")
