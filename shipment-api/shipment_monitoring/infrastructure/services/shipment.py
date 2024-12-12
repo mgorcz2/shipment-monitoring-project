b35adea86b07ee228aa43010ee89bb243a13039b
mgorczynsky2 <mgorczynsky2@gmail.com>
2024-12-12 14:02:21 +0100

    documentation

--- shipment-api/shipment_monitoring/infrastructure/services/shipment.py
+++ shipment-api/shipment_monitoring/infrastructure/services/shipment.py
@@ -1,3 +1,5 @@
+"""Module containing shipment service implementation."""
+
 from typing import Iterable, Any
 from shipment_monitoring.core.domain.shipment import Shipment
 from shipment_monitoring.core.repositories.ishipment import IShipmentRepository
@@ -11,6 +13,7 @@ from uuid import UUID
 
 
 class ShipmentService(IShipmentService):
+    """A class representing implementation of shipment-related services."""
     _repository: IShipmentRepository
 
     def __init__(self, repository: IShipmentRepository) -> None:
@@ -18,15 +21,38 @@ class ShipmentService(IShipmentService):
 
 
     async def get_shipment_by_id(self, shipment_id: int) -> ShipmentDTO | None:
+        """The method getting shipment by provided id.
+
+        Args:
+            shipment_id (int): The id of the shipment.
+
+        Returns:
+            ShipmentDTO | None: The shipment DTO details if exists.
+        """
         shipment = await self._repository.get_shipment_by_id(shipment_id)
         return ShipmentDTO.from_record(shipment)
     
     async def get_all_shipments(self) -> Iterable[ShipmentDTO]:
+        """The method getting all shipment from the repository.
+
+        Returns:
+            Iterable[ShipmentDTO]: The collection of the shipments.
+        """
         shipments = await self._repository.get_all_shipments()
         return [ShipmentDTO.from_record(shipment) for shipment in shipments]
 
     
     async def sort_by_distance(self, courier_location: Location, keyword) -> Iterable[ShipmentWithDistanceDTO]:
+        """The method sorting shipments by destination distance from courier.
+
+        Args:
+            courier_location (Location): Location of courier.
+            keyword (str): Sorting key.
+
+        Returns:
+            Iterable[ShipmentWithDistanceDTO]: Shipments with distance attribute sorted collection.
+        """
+
         shipments = await self._repository.get_all_shipments()
         courier_address = await geopy.get_address_from_location(courier_location)
         courier_coords = await geopy.get_coords(courier_address)
@@ -44,6 +70,15 @@ class ShipmentService(IShipmentService):
         return sorted_shipments
     
     async def add_shipment(self, data: ShipmentIn, user_id: UUID) -> ShipmentDTO | None:
+        """The method adding a shipment to the repository.
+        
+        Args:
+            shipment (ShipmentIn): The shipment input data.
+            user_id (UUID): UUID of the user(sender).
+
+        Returns:
+            ShipmentDTO | None: The newly added shipment if added.
+        """
         origin= await geopy.get_address_from_location(data.origin)
         destination = await geopy.get_address_from_location(data.destination)
         origin_coords = await geopy.get_coords(origin)
