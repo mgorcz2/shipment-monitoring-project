1c5ff4f523bf27ffb2de00af093f4faa799f48aa
mgorcz2 <mgorczynsky2@gmail.com>
2025-04-24 22:19:41 +0200

    add: test folder structure and first unit test

---
+++ shipment-api/shipment_monitoring/tests/unit/test_repositories/test_user_repository.py
@@ -0,0 +1,29 @@
+import pytest
+
+from shipment_monitoring.core.domain.user import UserIn
+from shipment_monitoring.db import database, init_db
+from shipment_monitoring.infrastructure.repositories.userdb import UserRepository
+
+
+@pytest.fixture(scope="session", autouse=True)
+def anyio_backend():
+    return "asyncio"
+
+
+@pytest.fixture(autouse=True)
+async def prepare_database():
+    await init_db()
+    await database.connect()
+    yield
+    await database.disconnect()
+
+
+@pytest.mark.anyio
+async def test_user_repository_crud():
+    repo = UserRepository()
+    assert await repo.get_user_by_username("alice") is None
+    alice = UserIn(username="alice", password="pwd", role="sender")
+    created = await repo.register_user(alice)
+    assert created.username == "alice"
+    fetched = await repo.get_user_by_username("alice")
+    assert fetched.username == "alice"
